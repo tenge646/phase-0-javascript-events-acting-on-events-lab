c4a7087647d1ca533664a800fdcd67f4e3c6a219
kinsman646 <teesam2004@gmail.com>
2023-09-29 09:54:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+
+let dodger;
+
+function moveDodgerLeft() {
+  const currentLeft = parseInt(dodger.style.left) || 0;
+  const newLeft = Math.max(0, currentLeft - 10); // Ensure the DODGER doesn't go beyond the left edge
+  dodger.style.left = newLeft + 'px';
+}
+
+function moveDodgerRight() {
+  const currentLeft = parseInt(dodger.style.left) || 0;
+  const maxLeft = window.innerWidth - dodger.clientWidth;
+  const newLeft = Math.min(maxLeft, currentLeft + 10); 
+  dodger.style.left = newLeft + 'px';
+}
+window.addEventListener('DOMContentLoaded', () => {
+  dodger = document.getElementById('dodger');
+  dodger.style.left = '180px'; 
+});
+
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = {
+    moveDodgerLeft,
+    moveDodgerRight,
+  };
+}
